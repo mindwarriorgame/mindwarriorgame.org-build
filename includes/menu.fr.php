9b773d24642bd4c54e79a00a62f2d2031041c9ae
stillwaiting <bgvozdev@gmail.com>
2024-10-17 15:04:25 +1100

    Build

---
+++ includes/menu.fr.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'quick-start' => 'Démarrage rapide',
+    'public-formulas' => 'Formules publiques',
+    'privacy-policy' => 'Politique de confidentialité',
+    'faq' => 'FAQ',
+    'purpose' => 'Quel est le but du jeu ?',
+    'formula' => 'Que dois-je écrire dans ma Formule ?',
+    'review' => 'Que se passe-t-il lorsque je passe en revue ma Formule ?',
+    'forgot' => 'Que se passe-t-il si j’oublie de passer en revue ma Formule ?',
+    'difficulty' => 'À quelle fréquence dois-je passer en revue ma Formule ?',
+    'pause' => 'Comment faire une pause sans pénalités ?',
+    'formula-formatting' => 'Puis-je utiliser le formatage de texte dans la Formule ?',
+    'controls' => 'Quels contrôles le jeu possède-t-il ?',
+    'name' => 'Pourquoi le jeu s’appelle-t-il "MindWarrior" ?',
+    'formula-example' => 'Puis-je voir les Formules des autres joueurs ?',
+    'share' => 'Puis-je partager ma Formule avec d’autres joueurs ?',
+    'privacy' => 'Qui pourrait voir ma Formule ?'
+);
